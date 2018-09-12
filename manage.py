1b4b96c5cb08b96cfc7b1eef7903233141758f92
Dmitri Kuksik <kuksikdmitri@gmail.com>
2018-09-12 23:24:13 +0200

    modify settings

--- manage.py
+++ manage.py
@@ -3,10 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    if os.environ.get('DJANGO_SETTINGS_MODE', '') == 'DOCKER':
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_movies.settings_docker')
-    else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_movies.settings_dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_movies.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
