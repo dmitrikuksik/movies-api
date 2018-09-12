e6f6a640b7aab23735d8ed5a6c3c8f153baccb93
Dmitri Kuksik <kuksikdmitri@gmail.com>
2018-09-12 23:32:05 +0200

    true debug heroku

--- api_movies/settings.py
+++ api_movies/settings.py
@@ -129,7 +129,7 @@ if os.getcwd() == '/app':
     }
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     ALLOWED_HOSTS = ['*']
-    DEBUG = False
+    DEBUG = True
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
     STATICFILES_DIRS = (
